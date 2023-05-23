216ec4e7fcba4404e43035bb5b5ade5ea63d970c
Shiqi Feng <feng.shiq@northeastern.edu>
2023-05-23 12:56:19 +0900

    a2 CSS and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 
 function App() {
-  return (
-      <div>
-        <h1>Web Dev Assignments</h1>
-        <ul>
-          <li><a href="labs/a1/index.html">Assignment 1 Labs</a></li>
-          <li><a href="tuiter/navigation.html">Tuiter</a></li>
-        </ul>
-      </div>
-  );
+    return (
+        <div>
+            <h1>Web Dev Assignments</h1>
+            <ul>
+                <li><a href="labs/a1/index.html">Assignment 1 Labs</a></li>
+                <li><a href="tuiter/navigation.html">Tuiter</a></li>
+                <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
