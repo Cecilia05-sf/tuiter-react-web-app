7bb7caf83c0f4701a3403a629b3f3816cc4e827a
Shiqi Feng <feng.shiq@northeastern.edu>
2023-06-07 12:02:33 +0800

    Explore page updated

--- src/tuiter/tuit-summary-list/index.js
+++ src/tuiter/tuit-summary-list/index.js
@@ -3,11 +3,11 @@ import {useSelector} from "react-redux";
 import TuitSummaryItem from "./tuit-summary-item";
 
 const TuitSummaryList = () => {
-    const { tuits } = useSelector(state => state.tuits)
+    const tuits  = useSelector(state => state.tuits)
     return(
         <ul className="list-group">
             {
-                tuits.map(tuit =>
+                tuits && tuits.map(tuit =>
                     <TuitSummaryItem key={tuit._id} tuit={tuit}/>
                 )
             }
